8a022ec1ac64bb97dc21fe8ad1216ac81ce646de
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-03-05 21:38:47 +0100

    docs: Update documentation configuration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,7 +38,8 @@ module.exports = {
       end: new Date().getFullYear()
     },
     repo: 'TypedProject/logger',
-    openCollective: 'tsed',
+    docsRepo: 'https://api.tsed.io/rest/github/TypedProject/logger',
+    openCollective: 'https://api.tsed.io/rest/opencollective',
     gitterUrl: 'https://gitter.im/Tsed-io/community',
     stackoverflowUrl: 'https://stackoverflow.com/search?q=tsed',
     sponsorUrl: 'https://opencollective.com/tsed',
