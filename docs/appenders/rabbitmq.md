f7fb921686bfa3fdac0badba91713cfd89b76f6c
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-03-11 09:24:08 +0100

    feat(rabbitmq): Add RabbitMQ appender package

---
+++ docs/appenders/rabbitmq.md
@@ -0,0 +1,50 @@
+# RabbitMQ Appender
+
+Push log events to a [RabbitMQ](https://www.rabbitmq.com/).
+
+```bash
+npm install --save @tsed/logger-rabitmq
+```
+
+If you want to be sure that all messages have been sent before your programme exits, remember to call `logger.shutdown()`.
+
+## Configuration
+
+* `type` - `rabbitmq`
+* `layout` - `object` (optional, defaults to `messagePassThroughLayout`) - the layout to use for log events (see [Layouts](/docs/layouts/readme.md)).
+* `options.host` - `string` (optional, defaults to `127.0.0.1`) - the location of the rabbitmq server
+* `options.port` - `integer` (optional, defaults to `5672`) - the port the rabbitmq server is listening on
+* `options.username` - `string` (optional, defaults to `guest`) - username to use when authenticating connection to rabbitmq
+* `options.password` - `string` (optional, defaults to `guest`) - password to use when authenticating connection to rabbitmq
+* `options.routing_key` - `string` (optional, defaults to `logstash`) - rabbitmq message's routing_key
+* `options.durable` - `string` (optional, defaults to false) - will that RabbitMQ lose our queue.
+* `options.exchange` - `string` (optional, defaults to `log`)- rabbitmq send message's exchange
+* `options.mq_type` - `string` (optional, defaults to `direct`) - rabbitmq message's mq_type
+* `options.vhost` - `string` (optional, defaults to `/`) - vhost to use
+* `options.shutdownTimeout` - `integer` (optional, defaults to `10000`) - maximum time in milliseconds to wait for messages to be sent during log4js shutdown.
+
+## Example
+
+```typescript
+import {Logger} from "@tsed/logger";
+import "@tsed/logger-rabbitmq"
+
+const logger = new Logger("loggerName");
+
+logger.appenders.set("stdout", {
+  type: "rabbitmq",
+  level: ["info"],
+  options: {
+    host: '127.0.0.1',
+    port: 5672,
+    username: 'guest',
+    password: 'guest',
+    routing_key: 'logstash',
+    exchange: 'exchange_logs',
+    mq_type: 'direct',
+    durable: true
+  }
+});
+```
+
+This configuration will push log messages to the rabbitmq on `127.0.0.1:5672`.
---
+++ packages/rabbitmq/src/RabbitMQAppender.ts
@@ -0,0 +1,124 @@
+import {$log, Appender, BaseAppender, LogEvent} from "@tsed/logger";
+import amqplib, {Connection} from "amqplib";
+
+@Appender({name: "seq"})
+export class RabbitMQAppender extends BaseAppender {
+  private connection: Connection;
+  private messagesToSend: any[] = [];
+  private promisesWaiting: number = 0;
+  private waitingToConnect: boolean = true;
+
+  get options() {
+    return {
+      host: "127.0.0.1",
+      port: 5672,
+      username: "guest",
+      password: "guest",
+      exchange: "log",
+      type: "direct",
+      durable: false,
+      routingKey: "logstash",
+      vhost: "/",
+      shutdownTimeout: 10000,
+      locale: "en_US",
+      ...this.config.options,
+      protocol: "amqp",
+      frameMax: 0,
+      heartbeat: 0,
+    };
+  }
+
+  build() {
+    if ($log.level !== "OFF") {
+      amqplib
+        .connect(this.options)
+        .then((c) => {
+          this.connection = c;
+          this.waitingToConnect = false;
+          // debug("Connected.");
+          this.publish();
+        })
+        .catch((e) => {
+          // debug("connect failed.");
+          this.waitingToConnect = false;
+          console.error(e); // eslint-disable-line
+        });
+    }
+  }
+
+  write(loggingEvent: LogEvent) {
+    if (loggingEvent.level.toString() !== "OFF") {
+      this.publish(this.layout(loggingEvent, this.config.timezoneOffset));
+    }
+  }
+
+  shutdown(): Promise<any> | void {
+    if (this.connection) {
+      return new Promise((resolve) => {
+        this.waitForPromises(resolve);
+      });
+    }
+  }
+
+  protected waiting() {
+    return this.waitingToConnect || this.promisesWaiting > 0 || this.messagesToSend.length > 0;
+  }
+
+  protected waitForPromises = (done: Function) => {
+    let howLongWaiting = 0;
+
+    const checker = async () => {
+      this.publish();
+      if (howLongWaiting >= this.options.shutdownTimeout) {
+        await this.closeConnection();
+        done();
+        return;
+      }
+      if (this.waiting()) {
+        howLongWaiting += 50;
+        setTimeout(checker, 50);
+      } else {
+        await this.closeConnection();
+        done();
+      }
+    };
+
+    checker();
+  };
+
+  protected publish(message?: string) {
+    if (message) {
+      this.messagesToSend.push(message);
+    }
+    if (!this.waitingToConnect && this.connection && this.messagesToSend.length > 0) {
+      this.send(this.messagesToSend);
+    }
+  }
+
+  protected send(messages: string[]) {
+    const {exchange, type, routingKey, durable} = this.options;
+    const rn = this.connection.createChannel().then((ch) => {
+      const ok = ch.assertExchange(exchange, type, {durable: durable});
+
+      return ok.then(() => {
+        messages.forEach((message) => {
+          ch.publish(exchange, routingKey, Buffer.from(message));
+        });
+        messages.length = 0;
+        return ch.close();
+      });
+    });
+
+    this.promisesWaiting += 1;
+
+    rn.then(() => {
+      this.promisesWaiting -= 1;
+    });
+  }
+
+  protected async closeConnection() {
+    if (this.connection) {
+      return this.connection.close();
+    }
+  }
+}
