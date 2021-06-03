76ed8b92b0b330279646e7b59e642d8f262db129
avalladaresm <avalladaresm@unitec.edu>
2021-06-03 00:16:15 -0600

    fix changed async to method level

--- packages/slack/src/SlackAppender.ts
+++ packages/slack/src/SlackAppender.ts
@@ -4,27 +4,24 @@ import {WebClient} from "@slack/web-api";
 
 @Appender({name: "slack"})
 export class SlackAppender extends BaseAppender {
-  write(loggingEvent: LogEvent) {
-    const {token, channel_id, icon_url, username} = this.config.options;
-    const level = loggingEvent.level.toString().toLowerCase();
-    const web = new WebClient(token);
-
-    if (level !== "off") {
-      const data = {
-        token,
-        channel: channel_id,
-        text: this.layout(loggingEvent, this.config.timezoneOffset),
-        icon_url,
-        username
-      };
-
-      (async () => {
-        try { await web.chat.postMessage(data); }
-        catch (err) {
-          console.error("Ts.ED Logger:slack - Error sending log to slack: ", err);
-        }
+  async write(loggingEvent: LogEvent) {
+    try {
+      const {token, channel_id, icon_url, username} = this.config.options;
+      const level = loggingEvent.level.toString().toLowerCase();
+      const web = new WebClient(token);
+      if (level !== "off") {
+        const data = {
+          token,
+          channel: channel_id,
+          text: this.layout(loggingEvent, this.config.timezoneOffset),
+          icon_url,
+          username
+        };
+        await web.chat.postMessage(data);
       }
-      )();
+    }
+    catch (err) {
+      console.error("Ts.ED Logger:slack - Error sending log to slack: ", err);
     }
   }
 }
