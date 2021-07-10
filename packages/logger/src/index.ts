05ed39b60465113ed3e4e778244df81b6529bc0e
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-07-10 08:24:25 +0200

    feat: Use microbundle to package logger and plugins
    Support modern ESM syntax with nodejs and legacy CommonJS module

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,18 +1,14 @@
+import {Logger} from "./logger/class/Logger";
+
 export * from "./core";
 export * from "./appenders";
 export * from "./layouts";
 export * from "./logger";
-
-import {Logger} from "./logger/class/Logger";
 
 let $log: Logger = new Logger("default");
 
 $log.appenders
   .set("stdout", {type: "stdout", levels: ["info", "debug"]})
   .set("stderr", {type: "stderr", levels: ["trace", "fatal", "error", "warn"]});
-
-export default {
-  $log
-};
 
 export {$log};
