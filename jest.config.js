33c8135a090fe627cb37c83e29fe512e3110a5ae
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-06 17:47:22 +0200

    chore: use typescript references

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
-
 export default {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
@@ -12,54 +11,74 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // moduleDirectories: ["node_modules", "packages"],
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["index.ts", "/node_modules/"],
+  coveragePathIgnorePatterns: ['index.ts', '/node_modules/'],
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-    "^@tsed/logger(.*)$": "<rootDir>/packages/logger$1/src"
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@tsed/logger(.*)$': '<rootDir>/packages/logger$1/src'
   },
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 77.67,
-      functions: 94.62,
-      lines: 96.06,
-      statements: 95.89
+      branches: 78.18,
+      functions: 90.42,
+      lines: 94.78,
+      statements: 94.46
     }
   },
 
   // An array of file extensions your modules use
-  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/packages/*/src/**/__tests__/**/*.[jt]s?(x)",
-    "**/packages/*/src/**/?(*.)+(spec|test).[tj]s?(x)",
-    "**/packages/*/test/**/?(*.)+(spec|test).[tj]s?(x)"
+    '**/packages/*/src/**/__tests__/**/*.[jt]s?(x)',
+    '**/packages/*/src/**/?(*.)+(spec|test).[tj]s?(x)',
+    '**/packages/*/test/**/?(*.)+(spec|test).[tj]s?(x)'
   ],
   // A map from regular expressions to paths to transformers
   transform: {
-    "\\.(ts)$": "ts-jest"
+    '^.+\\.(t|j)sx?$': ['@swc/jest', {
+      jsc: {
+        'target': 'es2021',
+        'parser': {
+          'syntax': 'typescript',
+          'tsx': true,
+          'decorators': true,
+          'dynamicImport': true,
+          'privateMethod': true,
+          'exportDefaultFrom': true,
+          'importMeta': true
+        },
+        'transform': {
+          'hidden': {
+            'jest': true
+          },
+          'legacyDecorator': true,
+          'decoratorMetadata': true
+        }
+      },
+      'module': {
+        'type': 'commonjs',
+        'strict': false,
+        'strictMode': true,
+        'lazy': false,
+        'noInterop': false
+      }
+    }
+    ]
   },
 
-  preset: "ts-jest",
-
-  modulePathIgnorePatterns: ["<rootDir>/packages/*/lib", "<rootDir>/dist"],
+  modulePathIgnorePatterns: ['<rootDir>/packages/*/lib', '<rootDir>/dist'],
   // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: [],
 
-  extensionsToTreatAsEsm: [".ts"],
-
-  globals: {
-    "ts-jest": {
-      useESM: true
-    }
-  }
-};
+  extensionsToTreatAsEsm: ['.ts']
+}
