7b546406a259c445fcc91d84abdcd5339394c43a
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-03-15 15:53:22 +0100

    fix(logger): fix exported symbol in web bundler

--- packages/logger/src/browser/exports.ts
+++ packages/logger/src/browser/exports.ts
@@ -1,5 +1,6 @@
 import {Logger} from "../common";
 
+export * from "../common";
 export const $log: Logger = new Logger("default");
 
 $log.appenders.set("console", {type: "console", levels: ["info", "debug", "trace", "fatal", "error", "warn"]});
